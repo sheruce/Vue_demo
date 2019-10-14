43ac951b6cb8b3031dc6abde2a6543e3c988c115
sheruce <sheruce@chess.com>
2019-10-14 10:17:06 +0800

    fstep

--- src/main.js
+++ src/main.js
@@ -1,34 +1,8 @@
 import Vue from 'vue'
-import App from 'app'
+import App from './App.vue'
 
 new Vue({
   el:'#app',
   components:{App},
   template:'<App/>'
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import Vue from 'vue'
-import App from './App'
-
-
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
 })
